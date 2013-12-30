a3df1c23f1b0569afa7e1997810ca293d649eaa5
Shlomi <shlomi.newmail@gmail.com>
2013-12-31 00:34:10 +0200

    Added tests for TimedResult

---
+++ UnitTests/TimedResultTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Utilities;
+
+namespace UnitTests
+{
+	[TestClass]
+	public class TimedResultTests
+	{
+		[TestMethod]
+		public void TimedResultTest()
+		{
+			var obj = new object();
+			var result = new TimedResult<object>(obj);
+
+			Assert.AreSame(obj, result.Result,
+				"result object is different that provided object");
+			Assert.IsFalse(result.DidTimedOut,
+				"result timed out, although object was provided");
+
+			result = new TimedResult<object>();
+
+			Assert.IsNull(result.Result,
+				"timed out result returned an object");
+			Assert.IsTrue(result.DidTimedOut,
+				"timed out result isn't timed out");
+		}
+	}
+}
