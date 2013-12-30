ed63b35abe3f22cf9eba66f853f9475ebc5c4b96
Shlomi Borovitz <shlomi.newmail@gmail.com>
2013-12-30 16:21:40 +0200

    Created tests for CLArguments

--- Utilities/CLArguments.cs
+++ Utilities/CLArguments.cs
@@ -126,14 +126,14 @@ namespace Utilities
 		/// <summary>
 		/// Gets or sets the values that stored under the given key.
 		/// </summary>
-		/// <param name="key"></param>
-		/// <returns></returns>
+		/// <param name="key">The key for the values.</param>
+		/// <returns>Values array of the values stored under the key.</returns>
 		public string[] this[string key]
 		{
 			get { return this.AsDict[key]; }
 			set
 			{
-				this.Add(key, value);
+				this.CreateOrReplace(key, value);
 			}
 		}
 
@@ -225,22 +225,8 @@ namespace Utilities
 		{
 			this.isKeyPred = isKeyPred;
 
-			this.asDict = new Dictionary<string, string[]>(keys);
-			this.AsList = new List<string>(keys.Count * 2);
-
-			if (keys.ContainsKey(NO_KEY))
-			{
-				this.AsList.AddRange(keys[NO_KEY]);
-			}
-
-			foreach (string key in keys.Keys)
-			{
-				if (key != NO_KEY)
-				{
-					this.AsList.Add(key);
-					this.AsList.AddRange(keys[key]);
-				}
-			}
+			this.asDict = null;
+			this.AsList = NormalizedListFromDict(keys, keys.Count * 2);
 		}
 		#endregion
 
@@ -283,6 +269,21 @@ namespace Utilities
 		}
 
 		/// <summary>
+		/// Searches for the specified object and returns the zero-based index of the
+		/// last occurrence within the entire list.
+		/// </summary>
+		/// <param name="arg">
+		/// The object to locate in the list.
+		/// </param>
+		/// <returns>
+		/// The zero-based index of the last occurrence of item within the entire the list, if found; otherwise, –1.
+		/// </returns>
+		public int LastIndexOf(string arg)
+		{
+			return this.AsList.LastIndexOf(arg);
+		}
+
+		/// <summary>
 		/// Returns the index of the given argument.
 		/// </summary>
 		/// <param name="arg">The argument to search.</param>
@@ -294,6 +295,24 @@ namespace Utilities
 		public int IndexOf(string arg, int startIndex)
 		{
 			return this.AsList.IndexOf(arg, startIndex);
+		}
+
+		/// <summary>
+		/// Searches for the specified object and returns the zero-based index of the
+		/// last occurrence within the range of elements in the list
+		/// that extends from the first element to the specified index.
+		/// </summary>
+		/// <param name="arg">
+		/// The object to locate in the List.
+		/// </param>
+		/// <param name="startIndex">The zero-based starting index of the backward search.</param>
+		/// <returns>
+		/// The zero-based index of the last occurrence of item within the range of elements
+		/// in the list that extends from the first element to index, if found; otherwise, –1.
+		/// </returns>
+		public int LastIndexOf(string arg, int startIndex)
+		{
+			return this.AsList.LastIndexOf(arg, startIndex);
 		}
 
 		/// <summary>
@@ -312,6 +331,25 @@ namespace Utilities
 		}
 
 		/// <summary>
+		/// Searches for the specified object and returns the zero-based index of the
+		/// last occurrence within the range of elements in the System.Collections.Generic.List<T>
+		/// that contains the specified number of elements and ends at the specified
+		/// index.
+		/// </summary>
+		/// <param name="arg">The object to locate in the list.</param>
+		/// <param name="startIndex">The zero-based starting index of the backward search.</param>
+		/// <param name="count">The number of elements in the section to search.</param>
+		/// <returns>
+		/// The zero-based index of the last occurrence of item within the range of elements
+		/// in the list that contains count number of elements
+		/// and ends at index, if found; otherwise, –1.
+		/// </returns>
+		public int LastIndexOf(string arg, int startIndex, int count)
+		{
+			return this.AsList.LastIndexOf(arg, startIndex, count);
+		}
+
+		/// <summary>
 		/// Inserts the given arg in the given index.
 		/// </summary>
 		/// <param name="index">The index which the arg will be inserted into.</param>
@@ -320,6 +358,35 @@ namespace Utilities
 		{
 			this.AsList.Insert(index, arg);
 			this.AsDict = null;
+		}
+
+		/// <summary>
+		/// Merges values from duplicate keys, and retain one values list per key.
+		/// </summary>
+		public void Normalize()
+		{
+			this.AsList = NormalizedListFromDict(this.AsDict, this.AsList.Count);
+		}
+
+		private List<string> NormalizedListFromDict(IDictionary<string, string[]> keys, int capacity)
+		{
+			var list = new List<string>(capacity);
+
+			if (keys.ContainsKey(NO_KEY))
+			{
+				list.AddRange(keys[NO_KEY]);
+			}
+
+			foreach (string key in keys.Keys)
+			{
+				if (key != NO_KEY)
+				{
+					list.Add(key);
+					list.AddRange(keys[key]);
+				}
+			}
+
+			return list;
 		}
 
 		private Dictionary<string, string[]> BuildDictionary()
@@ -512,6 +579,55 @@ namespace Utilities
 		}
 
 		/// <summary>
+		/// Creates or replace the given key, with the given values
+		/// </summary>
+		/// <param name="key">The key to create or to replace.</param>
+		/// <param name="value">The values for that key.</param>
+		public void CreateOrReplace(string key, string[] value)
+		{
+			if (ContainsKey(key))
+			{
+				RemoveKey(key);
+			}
+
+			Add(key, value);
+		}
+
+		/// <summary>
+		/// Removes the key, and all its values.
+		/// </summary>
+		/// <param name="key">The key to remove.</param>
+		/// <returns>true if the key was removed, with all its values; otherwise false.</returns>
+		public bool RemoveKey(string key)
+		{
+			bool isExist = ContainsKey(key);
+
+			if (isExist)
+			{
+				bool isCurrentKey = false;
+				var newList = new List<string>(this.AsList.Count);
+
+				for (int i = 0; i < this.Count; i++)
+				{
+					if (IsKey(this.AsList[i]))
+					{
+						isCurrentKey = this.AsList[i] == key;
+					}
+
+					if (!isCurrentKey)
+					{
+						newList.Add(this.AsList[i]);
+					}
+				}
+
+				this.AsList = newList;
+				this.asDict = null;
+			}
+
+			return isExist;
+		}
+
+		/// <summary>
 		/// Adds the given value args under the given key arg.
 		/// </summary>
 		/// <param name="key">The key arg.</param>
@@ -591,11 +707,34 @@ namespace Utilities
 		/// Returns the values under the specified key, if the key exists.
 		/// </summary>
 		/// <param name="key">The key for the values.</param>
-		/// <param name="value">Out: the values that was stored under the specified key.</param>
+		/// <param name="values">Out: the values that were stored under the specified key.</param>
 		/// <returns>true if the key exists, and the values have returned; otherwise false.</returns>
-		public bool TryGetValue(string key, out string[] value)
+		public bool TryGetValue(string key, out string[] values)
 		{
-			return this.AsDict.TryGetValue(key, out value);
+			return this.AsDict.TryGetValue(key, out values);
+		}
+
+		/// <summary>
+		/// Returns the values under the specified key, if the key exists, and if only one value is under it.
+		/// </summary>
+		/// <param name="key">The key for the value.</param>
+		/// <param name="value">Out: the value that was stored under the specified key.</param>
+		/// <returns>true if the key exists, is single-valued, and the values have returned; otherwise false.</returns>
+		public bool TryGetValue(string key, out string value)
+		{
+			string[] values;
+			value = null;
+
+			bool didSucceeded = this.AsDict.TryGetValue(key, out values) &&
+				values.Any() &&
+				values.Count() == 1;
+
+			if (didSucceeded)
+			{
+				value = values.Single();
+			}
+
+			return didSucceeded;
 		}
 
 		void ICollection<KeyValuePair<string, string[]>>.Add(KeyValuePair<string, string[]> item)
@@ -652,15 +791,17 @@ namespace Utilities
 			return Clone();
 		}
 
+		/// <summary>
+		/// Create a copy of the CLArguments
+		/// </summary>
+		/// <returns>A new copy of the object</returns>
 		public CLArguments Clone()
 		{
 			var clone = new CLArguments()
 			{
 				isKeyPred = this.isKeyPred,
 				AsList = new List<string>(this.AsList),
-				asDict = this.asDict != null
-					? new Dictionary<string, string[]>(this.asDict)
-					: null,
+				asDict = null,
 			};
 
 			return clone;
