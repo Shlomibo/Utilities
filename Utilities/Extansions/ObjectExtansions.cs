c06ede1a84d72191fe49f708ff9df236392f6f20
Shlomi Borovitz <shlomi.newmail@gmail.com>
2014-01-27 18:02:08 +0200

    Added extansions
    Added extansions to handle string nullness and emptiness

--- Utilities/Extansions/ObjectExtansions.cs
+++ Utilities/Extansions/ObjectExtansions.cs
@@ -236,5 +236,27 @@ namespace Utilities.Extansions
 		{
 			input.TryExecute(action);
 		}
+
+		/// <summary>
+		/// Indicates whether the specified string is null or an Empty string.
+		/// </summary>
+		/// <param name="str">The string to test.</param>
+		/// <returns>true if the value parameter is null or an empty string (""); otherwise, false.</returns>
+		public static bool IsNullOrEmpty(this string str)
+		{
+			return string.IsNullOrEmpty(str);
+		}
+
+		/// <summary>
+		/// Indicates whether a specified string is null, empty, or consists only of white-space characters.
+		/// </summary>
+		/// <param name="str">The string to test.</param>
+		/// <returns>
+		/// true if the value parameter is null or String.Empty, or if value consists exclusively of white-space characters. 
+		/// </returns>
+		public static bool IsNullOrWhiteSpace(this string str)
+		{
+			return string.IsNullOrWhiteSpace(str);
+		}
 	}
 }
